6ecacc722fb13a831c9a4017a0d4f5398d9784af
Abdulmalik <abdulmalik.litez@gmail.com>
2025-10-30 17:16:28 +0500

    text edited

--- lib/features/crypto_list/view/crypto_list_screen.dart
+++ lib/features/crypto_list/view/crypto_list_screen.dart
@@ -78,7 +78,7 @@ class _CryptoListScreenState extends State<CryptoListScreen> {
                     style: theme.textTheme.headlineMedium?.copyWith(color: Colors.white),
                     ),
                     Text(
-                    'Please try again later',
+                    'Please try again',
                     style: theme.textTheme.labelSmall?.copyWith(fontSize: 16),
                     ),
                 ],
